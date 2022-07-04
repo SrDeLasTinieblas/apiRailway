07b66f8c4cefe87778e94011855cfd06a885459b
SrDeLasTinieblas <52461757+SrDeLasTinieblas@users.noreply.github.com>
2022-07-03 21:25:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 
+
 app.use(async ctx => {
   ctx.body = 'Hello World from Railway';
 });
+
+app.get("/api",(req,res)=>{
+    res.json(
+        mensaje:"Nodejs and JWT"
+});
+
 const PORT = 5000
 
 app.listen(process.env.PORT || PORT, () => console.log("Corriendo en el puertoo ==> " + PORT));
